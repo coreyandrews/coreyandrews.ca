fca03511abd530f43a358e1dfc5f26dc542eeaa3
Corey <corey.andrews.ca@gmail.com>
2019-10-16 08:29:19 -0500

    Edited the frontpage to reflect new color and text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lara Tits</title>
+        <title>Corey Andrews</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -46,6 +46,7 @@
 
             .title {
                 font-size: 84px;
+                color: #135aa5;
             }
 
             .links > a {
@@ -81,18 +82,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Corey Andrews
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.linkedin.com/in/-coreyandrews/">LinkedIn</a>
+                    <a href="https://github.com/coreyandrews">GitHub</a>
+                    <a href="#">Folio</a>
+                    <a href="#">Resume</a>
+
                 </div>
             </div>
         </div>
